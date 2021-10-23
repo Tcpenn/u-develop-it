12b77ba5f9dc7a83dd165ed89a204d847e932134
Tcpenn <tcpenn1026@gmail.com>
2021-10-22 22:23:22 -0400

    added sql server connection and query to return table

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 //dependencies
 const express = require('express');
+const mysql = require('mysql2');
 
 //ports
 const PORT = process.env.PORT || 3001;
@@ -7,13 +8,23 @@ const app = express();
 
 //Express middleware
 app.use(express.urlencoded({ extended: false}));
-app.use(express.json);
+app.use(express.json());
 
-//get route to test connection
-app.get('/', (req, res) => {
-    res.join({
-        message: 'Hello World'
-    });
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'root',
+        //Your MySQL password
+        password: '264169Ty!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 //Default response for any other request (not found)
